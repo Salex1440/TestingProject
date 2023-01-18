3a688bc546413d416d11c458fafc7494104e5209
Salex <123@13.com>
2023-01-18 15:50:55 +0300

    Added some JPA annotations and constraints

--- src/main/java/com/example/TestingProject/customer/Customer.java
+++ src/main/java/com/example/TestingProject/customer/Customer.java
@@ -1,5 +1,6 @@
 package com.example.TestingProject.customer;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -10,23 +11,40 @@ import java.util.UUID;
 @Getter
 @Setter
 @Entity(name = "Customer")
-@Table(name = "customers")
+@Table(
+        name = "customers",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "customers_phone_number_unique",
+                        columnNames = { "phone_number" }
+                )
+        }
+)
+@JsonIgnoreProperties(value = {"id"}, allowGetters = true)
 public class Customer {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(
+            name = "name",
+            nullable = false
+    )
     @NotBlank
     private String name;
 
+    @Column(
+            name = "phone_number",
+            nullable = false
+    )
     @NotBlank
     private String phoneNumber;
 
     public Customer() {
     }
 
-    public Customer(UUID id, String name, String phoneNumber) {
-        this.id = id;
+    public Customer(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
