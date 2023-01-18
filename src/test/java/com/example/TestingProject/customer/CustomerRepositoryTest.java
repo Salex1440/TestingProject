eb02a3b4ea1990fe9a6bae7844663fdb386a10d5
Salex <123@13.com>
2023-01-18 14:34:51 +0300

    Added new tests

--- src/test/java/com/example/TestingProject/customer/CustomerRepositoryTest.java
+++ src/test/java/com/example/TestingProject/customer/CustomerRepositoryTest.java
@@ -2,15 +2,18 @@ package com.example.TestingProject.customer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.TransactionSystemException;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-
-@DataJpaTest
+@SpringBootTest
+@AutoConfigureTestDatabase
 class CustomerRepositoryTest {
 
     @Autowired
@@ -37,5 +40,45 @@ class CustomerRepositoryTest {
 //                    assertThat(c.getPhoneNumber()).isEqualTo(customer.getPhoneNumber());
                     assertThat(c).usingRecursiveComparison().isEqualTo(customer);
                 });
+    }
+
+    @Test
+    void notSaveCustomerWhenNameIsNull() {
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, null, "+1234");
+        assertThatThrownBy(() -> customerRepository.save(customer))
+                .hasMessageContaining("Could not commit JPA transaction")
+                .isInstanceOf(TransactionSystemException.class);
+
+    }
+
+    @Test
+    void notSaveCustomerWhenNameIsBlank() {
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "", "+1234");
+        assertThatThrownBy(() -> customerRepository.save(customer))
+                .hasMessageContaining("Could not commit JPA transaction")
+                .isInstanceOf(TransactionSystemException.class);
+
+    }
+
+    @Test
+    void notSaveCustomerWhenPhoneNumberIsNull() {
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "Abel", null);
+        assertThatThrownBy(() -> customerRepository.save(customer))
+                .hasMessageContaining("Could not commit JPA transaction")
+                .isInstanceOf(TransactionSystemException.class);
+
+    }
+
+    @Test
+    void notSaveCustomerWhenPhoneNumberIsBlank() {
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "Abel", "");
+        assertThatThrownBy(() -> customerRepository.save(customer))
+                .hasMessageContaining("Could not commit JPA transaction")
+                .isInstanceOf(TransactionSystemException.class);
+
     }
 }
