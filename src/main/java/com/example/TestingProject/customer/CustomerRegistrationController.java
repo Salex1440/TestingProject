4b005bd954ffbf1732b5ae9dd8e8382627dbc427
Salex <123@13.com>
2023-01-23 16:46:16 +0300

    Added logic to CustomerRegistrationController

--- src/main/java/com/example/TestingProject/customer/CustomerRegistrationController.java
+++ src/main/java/com/example/TestingProject/customer/CustomerRegistrationController.java
@@ -1,6 +1,7 @@
 package com.example.TestingProject.customer;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/customer-registration")
 public class CustomerRegistrationController {
 
+    private final CustomerRegistrationService customerRegistrationService;
+
+    @Autowired
+    public CustomerRegistrationController(CustomerRegistrationService customerRegistrationService) {
+        this.customerRegistrationService = customerRegistrationService;
+    }
+
     @PutMapping
     public void registerNewCustomer(
             @Valid @RequestBody CustomerRegistrationRequest request) {
-        System.out.println(request);
+        customerRegistrationService.registerNewCustomer(request);
     }
 }
