bc599c65853f7761feaa7a44c34832fb18e41f76
Salex <123@13.com>
2023-01-23 15:33:51 +0300

    Created MockStripeService for integration tests

---
+++ src/main/java/com/example/TestingProject/payment/stripe/MockStripeService.java
@@ -0,0 +1,24 @@
+package com.example.TestingProject.payment.stripe;
+
+import com.example.TestingProject.payment.CardPaymentCharge;
+import com.example.TestingProject.payment.CardPaymentCharger;
+import com.example.TestingProject.payment.Currency;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@ConditionalOnProperty(
+        value = "stripe.enabled",
+        havingValue = "false"
+)
+public class MockStripeService implements CardPaymentCharger {
+    @Override
+    public CardPaymentCharge chargeCard(String cardSource,
+                                        BigDecimal amount,
+                                        Currency currency,
+                                        String description) {
+        return new CardPaymentCharge(true);
+    }
+}
